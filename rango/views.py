d4f9738424f5206c2ad622c32fe2085e1e438c12
Tsang Yin Yuen <2642877y@student.gla.ac.uk>
2023-01-17 19:30:35 +0000

    milestone 3: django basics

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(_request):
+    return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+
+def about(_request):
+    return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
