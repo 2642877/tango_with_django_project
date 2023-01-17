4e86b0c5f40669c6cf42d1b0837a8d64c79eedae
Tsang Yin Yuen <2642877y@student.gla.ac.uk>
2023-01-17 23:04:13 +0000

    using templates and serving static media files

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(_request):
-    return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(_request):
     return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
